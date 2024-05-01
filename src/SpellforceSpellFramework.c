f76a433333033f9d24539aa4d15ebb0a3486d8b3
UnSchtalch <shadow.93.04@gmail.com>
2024-05-01 18:43:30 +0300

    Minor code cleanup

--- src/SpellforceSpellFramework.c
+++ src/SpellforceSpellFramework.c
@@ -1,23 +1,24 @@
 #include "api/sf_spell.h"
 #include "asi/sf_asi.h"
+uint16_t (__thiscall *get_spell_spell_line) (void *, uint16_t);
+uint32_t (__thiscall *figure_toolbox_get_unkn)(void *, uint16_t);
 
-uint16_t __thiscall addSpell(void *_this, uint16_t spell_id, uint16_t param2, SF_CGdTargetData *source, SF_CGdTargetData *target, uint16_t param5)
+uint16_t __thiscall addSpell(SF_CGdSpell *_this, uint16_t spell_id, uint16_t param2, SF_CGdTargetData *source, SF_CGdTargetData *target, uint16_t param5)
 {
 	uint16_t spell_index;
 	uint16_t spell_line;
 	if (target->entity_type == 1)
 	{
-		if (FALSE) //TODO call fun_06fe704 with _this+0x24 == 0
+		if (!figure_toolbox_get_unkn(_this->SF_CGdResource, target->entity_index))
 		{
 			return 0;
 		}
 	}
-	SF_GdSpell *active_spell_list = (SF_GdSpell *)((uint32_t)_this+0x56);
 
 	//find 1st free slot for spell
 	for (spell_index =1; spell_index < 799; ++spell_index)
 	{
-		if (active_spell_list[spell_index].spell_id == 0)
+		if (_this->active_spell_list[spell_index].spell_id == 0)
 		{
 			break;
 		}
@@ -27,25 +28,22 @@ uint16_t __thiscall addSpell(void *_this, uint16_t spell_id, uint16_t param2, SF
 		return 0;
 	}
 	//max_used spells update
-	if (*(uint16_t *)((uint32_t)_this+0x3C) < spell_index)
+	if (_this->max_used < spell_index)
 	{
-		*(uint16_t *)((uint32_t)_this+0x3C) = spell_index;
+		_this->max_used = spell_index;
 	}
-	active_spell_list[spell_index].spell_id = spell_id;
-	spell_line = 1; ///TODO call fun_066e100 with _this+0x3c as param
-	active_spell_list[spell_index].spell_line = spell_line;
-	active_spell_list[spell_index].source.entity_type = source->entity_type;
-	active_spell_list[spell_index].source.entity_index = source->entity_index;
-	active_spell_list[spell_index].source.position.X = (source->position).X;
-	active_spell_list[spell_index].source.position.Y = (source->position).Y;
-	active_spell_list[spell_index].target.entity_type = target->entity_type;
-	active_spell_list[spell_index].target.entity_index = target->entity_index;
-	active_spell_list[spell_index].target.position.X = (target->position).X;
-	active_spell_list[spell_index].target.position.Y = (target->position).Y;
-	//dirty AF
-	uint32_t unkn_ptr = *((uint32_t *)(((uint32_t)_this+0x4)));
-	uint16_t current_step = *(uint16_t *)(unkn_ptr+0xc);
-	active_spell_list[spell_index].to_do_count = param2 - current_step;
+	_this->active_spell_list[spell_index].spell_id = spell_id;
+	spell_line = get_spell_spell_line (_this->SF_CGdResource, spell_id);
+	_this->active_spell_list[spell_index].spell_line = spell_line;
+	_this->active_spell_list[spell_index].source.entity_type = source->entity_type;
+	_this->active_spell_list[spell_index].source.entity_index = source->entity_index;
+	_this->active_spell_list[spell_index].source.position.X = (source->position).X;
+	_this->active_spell_list[spell_index].source.position.Y = (source->position).Y;
+	_this->active_spell_list[spell_index].target.entity_type = target->entity_type;
+	_this->active_spell_list[spell_index].target.entity_index = target->entity_index;
+	_this->active_spell_list[spell_index].target.position.X = (target->position).X;
+	_this->active_spell_list[spell_index].target.position.Y = (target->position).Y;	
+	_this->active_spell_list[spell_index].to_do_count = param2 - (uint16_t)(_this->OpaqueClass->current_step);
 	//TODO Spell Line handler
 
 	return spell_index;
@@ -54,7 +52,9 @@ uint16_t __thiscall addSpell(void *_this, uint16_t spell_id, uint16_t param2, SF
 
 void hookBetaVersion()
 {
-
+	///this is optional, I'm lazy to make proper typedefs
+	get_spell_spell_line = (uint16_t(__thiscall *)(void*, uint16_t))(ASI::AddrOf(0x26E100));
+	figure_toolbox_get_unkn =(uint32_t(__thiscall *)(void*, uint16_t)) (ASI::AddrOf(0x2FE704));
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
