d59b17a47917226226cfd4e3e17d571fab8aa183
UnSchtalch <shadow.93.04@gmail.com>
2024-05-02 17:23:13 +0300

    Handlers up to 47

--- src/internal/SpellforceSpellHandler.cpp
+++ src/internal/SpellforceSpellHandler.cpp
@@ -35,9 +35,22 @@ void initSpellMap()
 	addSpellHandler(0x1e, &raise_dead_handler);
 	addSpellHandler(0x1f, &summons_handler);
 
-
+	addSpellHandler(0x20, &death_grasp_handler);
 	addSpellHandler(0x21, &summons_handler);
+	addSpellHandler(0x22, &inflexibility_handler);
+	addSpellHandler(0x23, &weaken_handler);
+	addSpellHandler(0x24, &dark_banishing_handler);
+	addSpellHandler(0x25, &area_slowness_handler);
+	addSpellHandler(0x26, &area_inflexibility_handler);
+	addSpellHandler(0x27, &area_weaken_handler);
+	addSpellHandler(0x28, &area_plague_handler);
+	addSpellHandler(0x29, &remediless_handler);
+	addSpellHandler(0x2a, &default_handler); //dark might
+	addSpellHandler(0x2b, &area_healing_handler);
+	addSpellHandler(0x2c, &sentinel_healing_handler);
 	addSpellHandler(0x2d, &healing_handler);
+	addSpellHandler(0x2e, &charm_animal_handler);
+	addSpellHandler(0x2f, &thorn_shield_handler);
 
 	addSpellHandler(0x5c, &summons_handler);
 	addSpellHandler(0x6a, &summons_handler);
@@ -225,3 +238,81 @@ void __thiscall raise_dead_handler (SF_CGdSpell* _this, uint16_t spell_index)
 {
 	_this->active_spell_list[spell_index].spell_job = 0x34;
 }
+
+void __thiscall death_grasp_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x1c;
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
+void __thiscall inflexibility_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x1d;
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
+void __thiscall weaken_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x1f;
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
+void __thiscall dark_banishing_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x20;
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
+void __thiscall area_slowness_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x21;
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
+void __thiscall area_inflexibility_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x22;
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
+void __thiscall area_weaken_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x23;
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
+void __thiscall area_plague_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x24;
+	setXData(_this, spell_index, SPELL_DOUBLE_DAMAGE, 0);
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
+void __thiscall remediless_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x25;
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
+void __thiscall area_healing_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x26;
+}
+
+void __thiscall sentinel_healing_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x27;
+}
+
+void __thiscall charm_animal_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x45;
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
+void __thiscall thorn_shield_handler (SF_CGdSpell* _this, uint16_t spell_index)
+{
+	_this->active_spell_list[spell_index].spell_job = 0x2a;
+	setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
+}
+
