a60ae77b693b91699e35be8baeccceba78b6cfba
UnSchtalch <shadow.93.04@gmail.com>
2024-05-17 00:58:28 +0300

    AOE Spell working, but requires more fine-tunning

--- examples/aoe_instant/aoe_instant.cpp
+++ examples/aoe_instant/aoe_instant.cpp
@@ -2,54 +2,60 @@
 #include "../api/sf_data_utilities.h"
 #include <windows.h>
 #include <stdio.h>
-//For convenience only; You can put headers just near the file and remove ../api/
+// For convenience only; You can put headers just near the file and remove ../api/
 
-//Another convenience: you can ommit functions you won't need, or call everything from the framework structure
+// Another convenience: you can ommit functions you won't need, or call everything from the framework structure
 SpellforceSpellFramework *sfsf;
 SpellFunctions *spellAPI;
 ToolboxFunctions *toolboxAPI;
 FigureFunctions *figureAPI;
 IteratorFunctions *iteratorAPI;
 
-void __thiscall aoe_lifetap_type_handler(SF_CGdSpell * _this, uint16_t spell_index) 
+void __thiscall aoe_lifetap_type_handler(SF_CGdSpell *_this, uint16_t spell_index)
 {
-	_this->active_spell_list[spell_index].spell_job = 0xf2; 
-
+    _this->active_spell_list[spell_index].spell_job = 0xf2;
 }
 
-
-
-void __thiscall aoe_lifetap_effect_handler(SF_CGdSpell * _this, uint16_t spell_index)
+void __thiscall aoe_lifetap_effect_handler(SF_CGdSpell *_this, uint16_t spell_index)
 {
     SF_GdSpell *spell = &_this->active_spell_list[spell_index];
     uint16_t source_index = spell->source.entity_index;
-    //Iterators are opaque from the user perspective. 
-    //Just give enough memory and don't bother what's inside
+    // Iterators are opaque from the user perspective.
+    // Just give enough memory and don't bother what's inside
     uint8_t iterator_memory[0x44];
     iteratorAPI->figureIteratorInit(&iterator_memory, 0x0, 0x0, 0x3ff, 0x3ff);
     iteratorAPI->figureIteratorSetPointers(&iterator_memory, _this->SF_CGdFigure, _this->unkn3, _this->SF_CGdWorld);
-    SF_CGdResourceSpell spell_data;
     SF_SpellEffectInfo effect_info;
+    SF_CGdResourceSpell spell_data;
+    effect_info.spell_id = spell->spell_id;
+    effect_info.job_id = spell->spell_job;
     SF_Rectangle hit_area;
-    SF_Coord cast_center = {0,0};
-    //SF_Coord
-    spellAPI->getResourceSpellData(_this->SF_CGdResource, &spell_data, effect_info.spell_id);
+    SF_Coord cast_center = _this->active_spell_list[spell_index].target.position;
+    // SF_Coord
+    spellAPI->getResourceSpellData(_this->SF_CGdResource, &spell_data, spell->spell_id);
     spellAPI->getTargetsRectangle(_this, &hit_area, spell_index, spell_data.params[0], &cast_center);
     SF_CGdTargetData relative_data;
     relative_data.position = cast_center;
     relative_data.entity_type = 4;
     relative_data.entity_index = 0;
-
     uint32_t unused;
-    spellAPI->addVisualEffect(_this,spell_index, 3, &unused, &relative_data, _this->OpaqueClass->current_step, 0x19, &hit_area);
-    iteratorAPI->iteratorSetArea(&iterator_memory,&cast_center, spell_data.params[0]);
+    spellAPI->addVisualEffect(_this, spell_index, 3, &unused, &relative_data, _this->OpaqueClass->current_step, 0x19, &hit_area);
+    iteratorAPI->iteratorSetArea(&iterator_memory, &cast_center, spell_data.params[0]);
     uint16_t target_index = iteratorAPI->figureIteratorGetNextFigure(&iterator_memory);
+
     while (target_index != 0)
     {
+        /* debug output example
+        Let it be here
+        char aliveInfo[256];
+        sprintf(aliveInfo, "Flags list: Target %hd \n", target_index);
+        sfsf->logInfo(aliveInfo);
+        */
         uint16_t random_roll = spellAPI->getRandom(_this->OpaqueClass, 100);
         uint32_t resist_chance = spellAPI->getChanceToResistSpell(_this->unkn2, source_index, target_index, effect_info);
         if ((uint32_t)resist_chance < random_roll)
         {
+            sfsf->logInfo("Resist OK");
             if (figureAPI->isAlive(_this->SF_CGdFigure, target_index))
             {
                 toolboxAPI->dealDamage(_this->SF_CGdFigureToolBox, source_index, target_index, spell_data.params[1], 1, 0, 0);
@@ -64,7 +70,7 @@ void __thiscall aoe_lifetap_effect_handler(SF_CGdSpell * _this, uint16_t spell_i
             }
             else
             {
-                spellAPI->figureAggro(_this, source_index, target_index);
+                spellAPI->figureAggro(_this, spell_index, target_index);
             }
         }
         target_index = iteratorAPI->figureIteratorGetNextFigure(&iterator_memory);
@@ -76,45 +82,42 @@ void __thiscall aoe_lifetap_effect_handler(SF_CGdSpell * _this, uint16_t spell_i
  * This function MUST be present in your code with the exact declaration
  * otherwise framework won't load your plugin
  ***/
-extern "C" __declspec(dllexport) void InitModule(SpellforceSpellFramework* framework) 
+extern "C" __declspec(dllexport) void InitModule(SpellforceSpellFramework *framework)
 {
-	sfsf = framework;
+    sfsf = framework;
     spellAPI = sfsf->apiSpellFunctions;
     toolboxAPI = sfsf->apiToolboxFunctions;
     figureAPI = sfsf->apiFigureFunctions;
     iteratorAPI = sfsf->apiIteratorFunctions;
-    //Here comes the spell type registration. 
-    //Note: you need to pass pointer to function here
+    // Here comes the spell type registration.
+    // Note: you need to pass pointer to function here
     sfsf->registerSpellTypeHandler(242, &aoe_lifetap_type_handler);
 
-
-    //Here comes the spell logic (effect) registration; f2 = 242
+    // Here comes the spell logic (effect) registration; f2 = 242
     sfsf->registerEffectHandler(0xf2, &aoe_lifetap_effect_handler);
-
 }
 
-
-//Required to be present, not required for any functionality
+// Required to be present, not required for any functionality
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-  switch (fdwReason)
-  {
+    switch (fdwReason)
+    {
     case DLL_PROCESS_ATTACH:
-      /* Code path executed when DLL is loaded into a process's address space. */
-      break;
+        /* Code path executed when DLL is loaded into a process's address space. */
+        break;
 
     case DLL_THREAD_ATTACH:
-      /* Code path executed when a new thread is created within the process. */
-      break;
+        /* Code path executed when a new thread is created within the process. */
+        break;
 
     case DLL_THREAD_DETACH:
-      /* Code path executed when a thread within the process has exited *cleanly*. */
-      break;
+        /* Code path executed when a thread within the process has exited *cleanly*. */
+        break;
 
     case DLL_PROCESS_DETACH:
-      /* Code path executed when DLL is unloaded from a process's address space. */
-      break;
-  }
+        /* Code path executed when DLL is unloaded from a process's address space. */
+        break;
+    }
 
-  return TRUE;
+    return TRUE;
 }
