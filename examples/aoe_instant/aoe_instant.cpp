70fa54e629952bedf459da67d40762b6a93bdbed
UnSchtalch <shadow.93.04@gmail.com>
2024-05-25 23:48:34 +0300

    Moved persisent aoe to new example, restored old code

--- examples/aoe_instant/aoe_instant.cpp
+++ examples/aoe_instant/aoe_instant.cpp
@@ -14,7 +14,6 @@ IteratorFunctions *iteratorAPI;
 void __thiscall aoe_lifetap_type_handler(SF_CGdSpell *_this, uint16_t spell_index)
 {
     _this->active_spell_list[spell_index].spell_job = 0xf2;
-    spellAPI->setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
 }
 
 void __thiscall aoe_lifetap_effect_handler(SF_CGdSpell *_this, uint16_t spell_index)
@@ -29,93 +28,67 @@ void __thiscall aoe_lifetap_effect_handler(SF_CGdSpell *_this, uint16_t spell_in
     // iteratorAPI->figureIteratorInit(&iterator_memory, 0x0, 0x0, 0x3ff, 0x3ff);
     // iteratorAPI->figureIteratorSetPointers(&iterator_memory, _this->SF_CGdFigure, _this->unkn3, _this->SF_CGdWorld);
 
+
     SF_SpellEffectInfo effect_info;
     SF_CGdResourceSpell spell_data;
-    spellAPI->getResourceSpellData(_this->SF_CGdResource, &spell_data, spell->spell_id);
-
     effect_info.spell_id = spell->spell_id;
     effect_info.job_id = spell->spell_job;
-    uint16_t ticks_total = spell_data.params[2];
-    uint16_t ticks_interval = spell_data.params[3];
     SF_Rectangle hit_area;
     SF_Coord cast_center = _this->active_spell_list[spell_index].target.position;
     // SF_Coord
+    spellAPI->getResourceSpellData(_this->SF_CGdResource, &spell_data, spell->spell_id);
     spellAPI->getTargetsRectangle(_this, &hit_area, spell_index, spell_data.params[0], &cast_center);
-
-    // uint32_t tick_left = spellAPI->getXData(_this, _this->active_spell_list[spell_index].xdata_key, SPELL_TICK_COUNT_AUX);
-    uint16_t ticks_passed = spellAPI->addToXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 1);
-
-    // visual effect addition, if it's 1st tick
-    if (ticks_passed == 1)
-    {
-        SF_CGdTargetData relative_data;
-        relative_data.position = cast_center;
-        relative_data.entity_type = 4;
-        relative_data.entity_index = 0;
-        uint32_t unused;
-        spellAPI->addVisualEffect(_this, spell_index, kGdEffectSpellHitWorld, &unused, &relative_data, _this->OpaqueClass->current_step, 0x19, &hit_area);
-    }
-
+    SF_CGdTargetData relative_data;
+    relative_data.position = cast_center;
+    relative_data.entity_type = 4;
+    relative_data.entity_index = 0;
+    uint32_t unused;
+    spellAPI->addVisualEffect(_this, spell_index, 3, &unused, &relative_data, _this->OpaqueClass->current_step, 0x19, &hit_area);
     iteratorAPI->iteratorSetArea(&iterator_memory, &cast_center, spell_data.params[0]);
     uint16_t target_index = iteratorAPI->figureIteratorGetNextFigure(&iterator_memory);
 
-    if (ticks_passed < ticks_total)
+    while (target_index != 0)
     {
-        while (target_index != 0)
+        //If figure is not special unit
+        //not dead and is not friendly
+        //and is targetable - let's work with it
+        if (((int16_t)(_this->SF_CGdFigure->figures[target_index].owner) != -1) &&
+            (((uint8_t)(_this->SF_CGdFigure->figures[target_index].flags) & 0xa) == 0) &&
+            (toolboxAPI->figuresCheckHostile(_this->SF_CGdFigureToolBox, source_index, target_index)) &&
+            (toolboxAPI->isTargetable(_this->SF_CGdFigureToolBox, target_index)))
         {
-            // If figure is not special unit
-            // not dead and is not friendly
-            // and is targetable - let's work with it
-            if (((int16_t)(_this->SF_CGdFigure->figures[target_index].owner) != -1) &&
-                (((uint8_t)(_this->SF_CGdFigure->figures[target_index].flags) & 0xa) == 0) &&
-                (toolboxAPI->figuresCheckHostile(_this->SF_CGdFigureToolBox, source_index, target_index)) &&
-                (toolboxAPI->isTargetable(_this->SF_CGdFigureToolBox, target_index)))
+            /* debug output example
+            Let it be here
+            char aliveInfo[256];
+            sprintf(aliveInfo, "Flags list: Target %hd \n", target_index);
+            sfsf->logInfo(aliveInfo);
+            */
+            uint16_t random_roll = spellAPI->getRandom(_this->OpaqueClass, 100);
+            uint32_t resist_chance = spellAPI->getChanceToResistSpell(_this->unkn2, source_index, target_index, effect_info);
+            if ((uint32_t)resist_chance < random_roll)
             {
-                /* debug output example
-                Let it be here
-                char aliveInfo[256];
-                sprintf(aliveInfo, "Flags list: Target %hd \n", target_index);
-                sfsf->logInfo(aliveInfo);
-                */
-                uint16_t random_roll = spellAPI->getRandom(_this->OpaqueClass, 100);
-                uint32_t resist_chance = spellAPI->getChanceToResistSpell(_this->unkn2, source_index, target_index, effect_info);
-                if ((uint32_t)resist_chance < random_roll)
+                if (figureAPI->isAlive(_this->SF_CGdFigure, target_index))
                 {
-                    if (figureAPI->isAlive(_this->SF_CGdFigure, target_index))
+                    toolboxAPI->dealDamage(_this->SF_CGdFigureToolBox, source_index, target_index, spell_data.params[1], 1, 0, 0);
+                    if (figureAPI->isAlive(_this->SF_CGdFigure, source_index))
                     {
-                        SF_CGdTargetData relative_data;
-                        relative_data.position = {0, 0};
-                        relative_data.entity_type = 1;
-                        relative_data.entity_index = target_index;
-                        uint32_t unused;
-                        SF_Rectangle aux_data;
-                        aux_data.partA = 0;
-                        aux_data.partB = 0;
-                        spellAPI->addVisualEffect(_this, spell_index, kGdEffectSpellHitTarget, &unused, &relative_data, _this->OpaqueClass->current_step, 10, &hit_area);
-                        toolboxAPI->dealDamage(_this->SF_CGdFigureToolBox, source_index, target_index, spell_data.params[1], 1, 0, 0);
-                        if (figureAPI->isAlive(_this->SF_CGdFigure, source_index))
+                        uint16_t current_hp = figureAPI->getCurrentHealth(_this->SF_CGdFigure, source_index);
+                        if (current_hp != 0)
                         {
-                            uint16_t current_hp = figureAPI->getCurrentHealth(_this->SF_CGdFigure, source_index);
-                            if (current_hp != 0)
-                            {
-                                figureAPI->decreaseHealth(_this->SF_CGdFigure, source_index, -spell_data.params[1]);
-                            }
+                            figureAPI->decreaseHealth(_this->SF_CGdFigure, source_index, -spell_data.params[1]);
                         }
                     }
-                    else
-                    {
-                        spellAPI->figureAggro(_this, spell_index, target_index);
-                    }
+                }
+                else
+                {
+                    spellAPI->figureAggro(_this, spell_index, target_index);
                 }
             }
-            target_index = iteratorAPI->figureIteratorGetNextFigure(&iterator_memory);
         }
-        _this->active_spell_list[spell_index].to_do_count = (uint16_t)((ticks_interval * 10) / 1000);
+        target_index = iteratorAPI->figureIteratorGetNextFigure(&iterator_memory);
     }
-    else
-    {
-        spellAPI->setEffectDone(_this, spell_index, 0);
-    }
+    spellAPI->setEffectDone(_this, spell_index, 0);
+
     iteratorAPI->disposeFigureIterator(iterator_memory);
 }
 
