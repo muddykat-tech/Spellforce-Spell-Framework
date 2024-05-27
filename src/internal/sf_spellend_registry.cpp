662f6b8584aabf928d5604518ac71e23b2463e0e
UnSchtalch <shadow.93.04@gmail.com>
2024-05-28 00:26:49 +0300

    Strength and agi buff spell end handlers

--- src/internal/sf_spellend_registry.cpp
+++ src/internal/sf_spellend_registry.cpp
@@ -64,6 +64,8 @@ void __thiscall weaken_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
 
 void __thiscall quickness_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
 {
+    SF_CGdResourceSpell spell_data;
+    apiSpellFunctions.getResourceSpellData(_this->SF_CGdResource, &spell_data, _this->active_spell_list[spell_index].spell_id);
     uint16_t target_index = _this->active_spell_list[spell_index].target.entity_index;
     int8_t bonus = (int8_t)(-apiSpellFunctions.getXData(_this, spell_index, SPELL_STAT_MUL_MODIFIER));
 
@@ -73,8 +75,35 @@ void __thiscall quickness_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
     }
     default_end_handler(_this, spell_index);
 }
-void __thiscall flexibility_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
-void __thiscall strength_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
+
+void __thiscall flexibility_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
+{
+    SF_CGdResourceSpell spell_data;
+    apiSpellFunctions.getResourceSpellData(_this->SF_CGdResource, &spell_data, _this->active_spell_list[spell_index].spell_id);
+    uint16_t target_index = _this->active_spell_list[spell_index].target.entity_index;
+    int8_t bonus = (int8_t)(-apiSpellFunctions.getXData(_this, spell_index, SPELL_STAT_MUL_MODIFIER));
+
+    if (!apiToolboxFunctions.hasSpellOnHit(_this->SF_CGdFigureToolBox, target_index, spell_data.spell_line_id))
+    {
+        apiFigureFunctions.addBonusMultToStatistic(_this->SF_CGdFigure, AGILITY, target_index, bonus);
+    }
+    default_end_handler(_this, spell_index);
+}
+
+void __thiscall strength_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
+{
+    SF_CGdResourceSpell spell_data;
+    apiSpellFunctions.getResourceSpellData(_this->SF_CGdResource, &spell_data, _this->active_spell_list[spell_index].spell_id);
+    uint16_t target_index = _this->active_spell_list[spell_index].target.entity_index;
+    int8_t bonus = (int8_t)(-apiSpellFunctions.getXData(_this, spell_index, SPELL_STAT_MUL_MODIFIER));
+
+    if (!apiToolboxFunctions.hasSpellOnHit(_this->SF_CGdFigureToolBox, target_index, spell_data.spell_line_id))
+    {
+        apiFigureFunctions.addBonusMultToStatistic(_this->SF_CGdFigure, STRENGTH, target_index, bonus);
+    }
+    default_end_handler(_this, spell_index);
+}
+
 void __thiscall retention_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
 void __thiscall brilliance_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
 void __thiscall suffocation_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
