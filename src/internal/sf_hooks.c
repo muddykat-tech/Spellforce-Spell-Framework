d08d313847f08074c8e144e537a4192e6cc4af96
UnSchtalch <shadow.93.04@gmail.com>
2024-05-27 14:16:30 +0300

    Add quickness, weaken, inflex, quicken

--- src/internal/sf_hooks.c
+++ src/internal/sf_hooks.c
@@ -23,7 +23,6 @@ uint32_t CMnuScrConsole_ptr = 0;
 SF_String_ctor_ptr SF_String_ctor;
 SF_String_dtor_ptr SF_String_dtor;
 
-
 SpellFunctions apiSpellFunctions;
 ToolboxFunctions apiToolboxFunctions;
 FigureFunctions apiFigureFunctions;
@@ -31,48 +30,52 @@ IteratorFunctions apiIteratorFunctions;
 
 int __thiscall CheckCanApply_hook_beta(SF_CGdSpell *_this, uint16_t spell_index)
 {
-    //We need a map of refresh handlers?
-    //And a default handler that does nothing
-    //handler takes (SF_CGdSpell *_this, uint16_t spell_index) as params
-
+    // We need a map of refresh handlers?
+    // And a default handler that does nothing
+    // handler takes (SF_CGdSpell *_this, uint16_t spell_index) as params
 }
 
 void __thiscall EndSpell_hook_beta(SF_CGdSpell *_this, uint16_t spell_index)
 {
-    //We need a map of end spell handlers?
-    //And a default handler that does nothing
-    //handler takes (SF_CGdSpell *_this, uint16_t spell_index) as params
+    // We need a map of end spell handlers?
+    // And a default handler that does nothing
+    // handler takes (SF_CGdSpell *_this, uint16_t spell_index) as params
 }
 
-
-void __thiscall triggerEffect_hook(SF_CGdSpell *_this){
-	uint16_t spell_index;
-	for (spell_index = 1; spell_index <= _this->max_used; ++spell_index)
+void __thiscall triggerEffect_hook(SF_CGdSpell *_this)
+{
+    uint16_t spell_index;
+    for (spell_index = 1; spell_index <= _this->max_used; ++spell_index)
     {
         if (_this->active_spell_list[spell_index].spell_id != 0)
         {
             short to_do_count = _this->active_spell_list[spell_index].to_do_count;
-            if(to_do_count == 0){
-				to_do_count = 0;
-			} else {
-				to_do_count = to_do_count + -1;
-			}
-			_this->active_spell_list[spell_index].to_do_count = to_do_count;
-			if(to_do_count == 0){
-				uint16_t spell_job = _this->active_spell_list[spell_index].spell_job;
-				handler_ptr func = get_spell_effect(spell_job);
-				if(func != NULL) {
-					func(_this, spell_index);
-				}
-			}
+            if (to_do_count == 0)
+            {
+                to_do_count = 0;
+            }
+            else
+            {
+                to_do_count = to_do_count + -1;
+            }
+            _this->active_spell_list[spell_index].to_do_count = to_do_count;
+            if (to_do_count == 0)
+            {
+                uint16_t spell_job = _this->active_spell_list[spell_index].spell_job;
+                handler_ptr func = get_spell_effect(spell_job);
+                if (func != NULL)
+                {
+                    func(_this, spell_index);
+                }
+            }
         }
     }
 }
 
-
-void ConsoleLog(const char* message)
+void ConsoleLog(const char *message)
 {
-	if (!CMnuScrConsole_ptr) return;
+    if (!CMnuScrConsole_ptr)
+        return;
     SF_String sf_string;
     SF_String_ctor(&sf_string, message);
     ConsolePrint(CMnuScrConsole_ptr, &sf_string);
@@ -81,67 +84,69 @@ void ConsoleLog(const char* message)
 
 uint16_t __thiscall addSpell_hook_beta(SF_CGdSpell *_this, uint16_t spell_id, uint16_t param2, SF_CGdTargetData *source, SF_CGdTargetData *target, uint16_t param5)
 {
-	uint16_t spell_index;
-	uint16_t spell_line;
-	if (target->entity_type == 1)
-	{
-		if (!figure_toolbox_get_unkn(_this->SF_CGdFigureToolBox, target->entity_index))
-		{
-			return 0;
-		}
-	}
+    uint16_t spell_index;
+    uint16_t spell_line;
+    if (target->entity_type == 1)
+    {
+        if (!figure_toolbox_get_unkn(_this->SF_CGdFigureToolBox, target->entity_index))
+        {
+            return 0;
+        }
+    }
 
-	//find 1st free slot for spell
-	for (spell_index =1; spell_index < 799; ++spell_index)
-	{
-		if (_this->active_spell_list[spell_index].spell_id == 0)
-		{
-			break;
-		}
-	}
-	if (spell_index == 799)
-	{
-		logWarning("Maximum spell limit (800) reached. Additional spells may not be registered.");
-		return 0;
-	}
-	//max_used spells update
-	if (_this->max_used < spell_index)
-	{
-		_this->max_used = spell_index;
-	}
+    // find 1st free slot for spell
+    for (spell_index = 1; spell_index < 799; ++spell_index)
+    {
+        if (_this->active_spell_list[spell_index].spell_id == 0)
+        {
+            break;
+        }
+    }
+    if (spell_index == 799)
+    {
+        logWarning("Maximum spell limit (800) reached. Additional spells may not be registered.");
+        return 0;
+    }
+    // max_used spells update
+    if (_this->max_used < spell_index)
+    {
+        _this->max_used = spell_index;
+    }
 
-	//somewhere here is smth goes south
-	_this->active_spell_list[spell_index].spell_id = spell_id;
-	spell_line = get_spell_spell_line (_this->SF_CGdResource, spell_id);
-	_this->active_spell_list[spell_index].spell_line = spell_line;
-	_this->active_spell_list[spell_index].source.entity_type = source->entity_type;
-	_this->active_spell_list[spell_index].source.entity_index = source->entity_index;
-	_this->active_spell_list[spell_index].source.position.X = (source->position).X;
-	_this->active_spell_list[spell_index].source.position.Y = (source->position).Y;
-	_this->active_spell_list[spell_index].target.entity_type = target->entity_type;
-	_this->active_spell_list[spell_index].target.entity_index = target->entity_index;
-	_this->active_spell_list[spell_index].target.position.X = (target->position).X;
-	_this->active_spell_list[spell_index].target.position.Y = (target->position).Y;
-	_this->active_spell_list[spell_index].to_do_count = param2 - (uint16_t)(_this->OpaqueClass->current_step);
+    // somewhere here is smth goes south
+    _this->active_spell_list[spell_index].spell_id = spell_id;
+    spell_line = get_spell_spell_line(_this->SF_CGdResource, spell_id);
+    _this->active_spell_list[spell_index].spell_line = spell_line;
+    _this->active_spell_list[spell_index].source.entity_type = source->entity_type;
+    _this->active_spell_list[spell_index].source.entity_index = source->entity_index;
+    _this->active_spell_list[spell_index].source.position.X = (source->position).X;
+    _this->active_spell_list[spell_index].source.position.Y = (source->position).Y;
+    _this->active_spell_list[spell_index].target.entity_type = target->entity_type;
+    _this->active_spell_list[spell_index].target.entity_index = target->entity_index;
+    _this->active_spell_list[spell_index].target.position.X = (target->position).X;
+    _this->active_spell_list[spell_index].target.position.Y = (target->position).Y;
+    _this->active_spell_list[spell_index].to_do_count = param2 - (uint16_t)(_this->OpaqueClass->current_step);
 
-	handler_ptr func = get_spell_handler(spell_line);
-	func (_this, spell_index);
+    handler_ptr func = get_spell_handler(spell_line);
+    func(_this, spell_index);
 
-	if (target->entity_type == 1)
-	{
-		if (spell_line != 0x5A)
-		{
-			figure_toolbox_add_spell (_this->SF_CGdFigureToolBox, target->entity_index, spell_index);
-		}
-	}
-	return spell_index;
+    if (target->entity_type == 1)
+    {
+        if (spell_line != 0x5A)
+        {
+            figure_toolbox_add_spell(_this->SF_CGdFigureToolBox, target->entity_index, spell_index);
+        }
+    }
+    return spell_index;
 }
 
 // May be best to remove this function and deal with it elsewhere.
-void __thiscall addBonusMultToStatistic(SF_CGdFigure* figure, StatisticDataKey key, uint16_t target, uint8_t value){
-	bool invalid = FALSE;
-	FigureStatistic statistic;
-	switch (key) {
+void __thiscall addBonusMultToStatistic(SF_CGdFigure *figure, StatisticDataKey key, uint16_t target, int8_t value)
+{
+    bool invalid = FALSE;
+    FigureStatistic statistic;
+    switch (key)
+    {
     case ARMOR:
         statistic = figure->figures[target].armor;
         break;
@@ -157,7 +162,7 @@ void __thiscall addBonusMultToStatistic(SF_CGdFigure* figure, StatisticDataKey k
     case INTELLIGENCE:
         statistic = figure->figures[target].intelligence;
         break;
-    case STRENGHT:
+    case STRENGTH:
         statistic = figure->figures[target].strength;
         break;
     case WISDOM:
@@ -186,74 +191,80 @@ void __thiscall addBonusMultToStatistic(SF_CGdFigure* figure, StatisticDataKey k
         break;
     default:
         // Handle default case if needed
-		invalid = TRUE;
+        invalid = TRUE;
         break;
-	}
+    }
 
-	if(invalid) 
-	{
-		logWarning("INVALID STATISTIC KEY");
-		return;
-	}
+    if (invalid)
+    {
+        logWarning("INVALID STATISTIC KEY");
+        return;
+    }
 
-	apiFigureFunctions.addBonusMult(statistic, value);
-	return;
+    apiFigureFunctions.addBonusMult(statistic, value);
+    return;
 }
 
-void initConsoleHook(){
-	uint32_t CAppMain_ptr = ASI::AddrOf(0x9229A8);
-	uint32_t CAppMenu_ptr = *(uint32_t*) (CAppMain_ptr + 0x4);
-	uint32_t CMnuScrConsole_ptr = *(uint32_t*) (CAppMenu_ptr + 0x80);
-	ConsolePrint = (ConsolePrint_ptr) ASI::AddrOf(0x534e70);
-	SF_String_ctor =(SF_String_ctor_ptr) ASI::AddrOf(0x3837e0);
-	SF_String_dtor =(SF_String_dtor_ptr) ASI::AddrOf(0x3839c0);
+void initConsoleHook()
+{
+    uint32_t CAppMain_ptr = ASI::AddrOf(0x9229A8);
+    uint32_t CAppMenu_ptr = *(uint32_t *)(CAppMain_ptr + 0x4);
+    uint32_t CMnuScrConsole_ptr = *(uint32_t *)(CAppMenu_ptr + 0x80);
+    ConsolePrint = (ConsolePrint_ptr)ASI::AddrOf(0x534e70);
+    SF_String_ctor = (SF_String_ctor_ptr)ASI::AddrOf(0x3837e0);
+    SF_String_dtor = (SF_String_dtor_ptr)ASI::AddrOf(0x3839c0);
 }
 
-void __thiscall setupFigureIterator(CGdFigureIterator *iterator, SF_CGdSpell *spell){
+void __thiscall setupFigureIterator(CGdFigureIterator *iterator, SF_CGdSpell *spell)
+{
     apiIteratorFunctions.figureIteratorInit(iterator, 0x0, 0x0, 0x3ff, 0x3ff);
     apiIteratorFunctions.figureIteratorSetPointers(iterator, spell->SF_CGdFigure, spell->unkn3, spell->SF_CGdWorld);
 }
 
 // Some funky stuff to clean up Iterator memory, not 100% if correct
-void __thiscall disposeFigureIterator(CGdFigureIterator iterator ){
+void __thiscall disposeFigureIterator(CGdFigureIterator iterator)
+{
     uint32_t unused;
-    FUN_0069eaf0(&iterator.data.offset_0x30, &unused, ((AutoClass69 *) iterator.data.offset_0x30.ac69_ptr1)->ac69_ptr1, iterator.data.offset_0x30.ac69_ptr1);
+    FUN_0069eaf0(&iterator.data.offset_0x30, &unused, ((AutoClass69 *)iterator.data.offset_0x30.ac69_ptr1)->ac69_ptr1, iterator.data.offset_0x30.ac69_ptr1);
     fidFree(iterator.data.offset_0x30.ac69_ptr1);
 }
 
-void initDataHooks(){
-	// Required for internal use
-	get_spell_spell_line = (get_spell_spell_line_ptr) (ASI::AddrOf(0x26E100));
-	figure_toolbox_get_unkn = (figure_toolbox_get_unkn_ptr) (ASI::AddrOf(0x2FE704));
-	figure_toolbox_add_spell = (figure_toolbox_add_spell_ptr) (ASI::AddrOf(0x2F673A));
-	
-	//used in Iterator for AOE Spells Dispose
-	FUN_0069eaf0 = (FUN_0069eaf0_ptr)(ASI::AddrOf(0x29EAF0));
-	fidFree = (fidfree_ptr)(ASI::AddrOf(0x6B6E25));
+void initDataHooks()
+{
+    // Required for internal use
+    get_spell_spell_line = (get_spell_spell_line_ptr)(ASI::AddrOf(0x26E100));
+    figure_toolbox_get_unkn = (figure_toolbox_get_unkn_ptr)(ASI::AddrOf(0x2FE704));
+    figure_toolbox_add_spell = (figure_toolbox_add_spell_ptr)(ASI::AddrOf(0x2F673A));
 
-	// More defined for external use in api
-	DEFINE_FUNCTION(Figure, isAlive, 0x1BE4D0);
-	DEFINE_FUNCTION(Figure, setWalkSpeed, 0x2B7190);
-	DEFINE_FUNCTION(Figure, addAction, 0x2AE0B0);
-	DEFINE_FUNCTION(Figure, addBonusMult, 0x35A3E0);
+    // used in Iterator for AOE Spells Dispose
+    FUN_0069eaf0 = (FUN_0069eaf0_ptr)(ASI::AddrOf(0x29EAF0));
+    fidFree = (fidfree_ptr)(ASI::AddrOf(0x6B6E25));
+
+    // More defined for external use in api
+    DEFINE_FUNCTION(Figure, isAlive, 0x1BE4D0);
+    DEFINE_FUNCTION(Figure, setWalkSpeed, 0x2B7190);
+    DEFINE_FUNCTION(Figure, addAction, 0x2AE0B0);
+    DEFINE_FUNCTION(Figure, addBonusMult, 0x35A3E0);
     DEFINE_FUNCTION(Figure, decreaseHealth, 0x2b5b40);
     DEFINE_FUNCTION(Figure, getCurrentHealth, 0x279350);
 
-	// Define the function pointers for SpellFunctions group
-	DEFINE_FUNCTION(Spell, setXData, 0x329C40);
-	DEFINE_FUNCTION(Spell, setEffectDone, 0x32A730);
-	DEFINE_FUNCTION(Spell, addToXData, 0x32AAC0);
-	DEFINE_FUNCTION(Spell, getChanceToResistSpell, 0x317BA0);
-	DEFINE_FUNCTION(Spell, getRandom, 0x2AD200);
-	DEFINE_FUNCTION(Spell, addVisualEffect, 0x329B30);
-	DEFINE_FUNCTION(Spell, figureAggro, 0x329c90);
-	DEFINE_FUNCTION(Spell, getResourceSpellData, 0x26dc40);
-	DEFINE_FUNCTION(Spell, getXData, 0x32aa60);
+    // Define the function pointers for SpellFunctions group
+    DEFINE_FUNCTION(Spell, setXData, 0x329C40);
+    DEFINE_FUNCTION(Spell, setEffectDone, 0x32A730);
+    DEFINE_FUNCTION(Spell, addToXData, 0x32AAC0);
+    DEFINE_FUNCTION(Spell, getChanceToResistSpell, 0x317BA0);
+    DEFINE_FUNCTION(Spell, getRandom, 0x2AD200);
+    DEFINE_FUNCTION(Spell, addVisualEffect, 0x329B30);
+    DEFINE_FUNCTION(Spell, figureAggro, 0x329c90);
+    DEFINE_FUNCTION(Spell, getResourceSpellData, 0x26dc40);
+    DEFINE_FUNCTION(Spell, getXData, 0x32aa60);
     DEFINE_FUNCTION(Spell, getTargetsRectangle, 0x329D80);
 
-	DEFINE_FUNCTION(Toolbox, dealDamage, 0x2f4a57);
-	DEFINE_FUNCTION(Toolbox, isTargetable, 0x2fe704);
-	DEFINE_FUNCTION(Toolbox, figuresCheckHostile, 0x2fe7b9);
+    DEFINE_FUNCTION(Toolbox, dealDamage, 0x2f4a57);
+    DEFINE_FUNCTION(Toolbox, isTargetable, 0x2fe704);
+    DEFINE_FUNCTION(Toolbox, figuresCheckHostile, 0x2fe7b9);
+    DEFINE_FUNCTION(Toolbox, hasSpellOnHit, 0x2fe4ea);
+
     DEFINE_FUNCTION(Toolbox, buildingDealDamage, 0x2d6d80);
 
     DEFINE_FUNCTION(Iterator, figureIteratorInit, 0x3183f0);
@@ -261,40 +272,41 @@ void initDataHooks(){
     DEFINE_FUNCTION(Iterator, iteratorSetArea, 0x3195d0);
     DEFINE_FUNCTION(Iterator, figureIteratorGetNextFigure, 0x318f50);
 
-	// Method to include functions WE define in the Internal code.
-	INCLUDE_FUNCTION(Spell, initializeSpellData, &initializeSpellData);
-	INCLUDE_FUNCTION(Figure, addBonusMultToStatistic, &addBonusMultToStatistic);
+    // Method to include functions WE define in the Internal code.
+    INCLUDE_FUNCTION(Spell, initializeSpellData, &initializeSpellData);
+    INCLUDE_FUNCTION(Figure, addBonusMultToStatistic, &addBonusMultToStatistic);
 
-	INCLUDE_FUNCTION(Iterator, setupFigureIterator, &setupFigureIterator);
-	INCLUDE_FUNCTION(Iterator, disposeFigureIterator, &disposeFigureIterator);
+    INCLUDE_FUNCTION(Iterator, setupFigureIterator, &setupFigureIterator);
+    INCLUDE_FUNCTION(Iterator, disposeFigureIterator, &disposeFigureIterator);
 }
 
-void initSpellTypeHook(){
-	ASI::MemoryRegion add_spell_mreg(ASI::AddrOf(0x328d60), 5);
+void initSpellTypeHook()
+{
+    ASI::MemoryRegion add_spell_mreg(ASI::AddrOf(0x328d60), 5);
     ASI::BeginRewrite(add_spell_mreg);
-        *(unsigned char*)(ASI::AddrOf(0x328d60)) = 0xE9;   // jmp instruction
-        *(int*)(ASI::AddrOf(0x328d61)) = (int)(&addSpell_hook_beta) - ASI::AddrOf(0x328d65);
+    *(unsigned char *)(ASI::AddrOf(0x328d60)) = 0xE9; // jmp instruction
+    *(int *)(ASI::AddrOf(0x328d61)) = (int)(&addSpell_hook_beta) - ASI::AddrOf(0x328d65);
     ASI::EndRewrite(add_spell_mreg);
 }
 
 void initSpellTriggerHook()
 {
-	ASI::MemoryRegion add_spell_mreg(ASI::AddrOf(0x278773), 5);
+    ASI::MemoryRegion add_spell_mreg(ASI::AddrOf(0x278773), 5);
     ASI::BeginRewrite(add_spell_mreg);
-        *(unsigned char*)(ASI::AddrOf(0x278773)) = 0xE8;   // CALL instruction
-        *(int*)(ASI::AddrOf(0x278774)) = (int)(&triggerEffect_hook) - ASI::AddrOf(0x278778);
+    *(unsigned char *)(ASI::AddrOf(0x278773)) = 0xE8; // CALL instruction
+    *(int *)(ASI::AddrOf(0x278774)) = (int)(&triggerEffect_hook) - ASI::AddrOf(0x278778);
     ASI::EndRewrite(add_spell_mreg);
 }
 
 // Exposed in sf_hooks.h
 void initBetaHooks()
 {
-	logInfo("Hooking Game Console");
-	initConsoleHook();
+    logInfo("Hooking Game Console");
+    initConsoleHook();
 
-	logInfo("Hooking Spell Types");
-	initSpellTypeHook();
-	
-	logInfo("Hooking Spell Triggers");
-	initSpellTriggerHook();
+    logInfo("Hooking Spell Types");
+    initSpellTypeHook();
+
+    logInfo("Hooking Spell Triggers");
+    initSpellTriggerHook();
 }
--- src/internal/sf_spellend_registry.cpp
+++ src/internal/sf_spellend_registry.cpp
@@ -16,9 +16,10 @@ void __thiscall slowness_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
     apiSpellFunctions.getResourceSpellData(_this->SF_CGdResource, &spell_data, _this->active_spell_list[spell_index].spell_id);
     uint16_t target_index = _this->active_spell_list[spell_index].target.entity_index;
 
-    // if (apiToolboxFunctions.hasSpellOnHit(_this->SF_CGdFigureToolBox, target_index, spell_data.spell_line_id)) {
-    apiFigureFunctions.addBonusMultToStatistic(_this->SF_CGdFigure, WALK_SPEED, target_index, spell_data.params[0]);
-    //}
+    if (apiToolboxFunctions.hasSpellOnHit(_this->SF_CGdFigureToolBox, target_index, spell_data.spell_line_id))
+    {
+        apiFigureFunctions.addBonusMultToStatistic(_this->SF_CGdFigure, WALK_SPEED, target_index, spell_data.params[0]);
+    }
     // just don't wanna to repeat it every time
     default_end_handler(_this, spell_index);
 }
@@ -28,15 +29,50 @@ void __thiscall decay1_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
     apiSpellFunctions.getResourceSpellData(_this->SF_CGdResource, &spell_data, _this->active_spell_list[spell_index].spell_id);
     uint16_t target_index = _this->active_spell_list[spell_index].target.entity_index;
 
-    // if (!apiToolboxFunctions.hasSpellOnHit(_this->SF_CGdFigureToolBox, target_index, spell_data.spell_line_id)) {
-    apiFigureFunctions.addBonusMultToStatistic(_this->SF_CGdFigure, ARMOR, target_index, spell_data.params[2]);
-    //}
+    if (!apiToolboxFunctions.hasSpellOnHit(_this->SF_CGdFigureToolBox, target_index, spell_data.spell_line_id))
+    {
+        apiFigureFunctions.addBonusMultToStatistic(_this->SF_CGdFigure, ARMOR, target_index, spell_data.params[2]);
+    }
     default_end_handler(_this, spell_index);
 }
 
-void __thiscall inflexibility_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
-void __thiscall weaken_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
-void __thiscall quickness_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
+void __thiscall inflexibility_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
+{
+    SF_CGdResourceSpell spell_data;
+    apiSpellFunctions.getResourceSpellData(_this->SF_CGdResource, &spell_data, _this->active_spell_list[spell_index].spell_id);
+    uint16_t target_index = _this->active_spell_list[spell_index].target.entity_index;
+
+    if (!apiToolboxFunctions.hasSpellOnHit(_this->SF_CGdFigureToolBox, target_index, spell_data.spell_line_id))
+    {
+        apiFigureFunctions.addBonusMultToStatistic(_this->SF_CGdFigure, AGILITY, target_index, spell_data.params[0]);
+    }
+    default_end_handler(_this, spell_index);
+}
+
+void __thiscall weaken_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
+{
+    SF_CGdResourceSpell spell_data;
+    apiSpellFunctions.getResourceSpellData(_this->SF_CGdResource, &spell_data, _this->active_spell_list[spell_index].spell_id);
+    uint16_t target_index = _this->active_spell_list[spell_index].target.entity_index;
+
+    if (!apiToolboxFunctions.hasSpellOnHit(_this->SF_CGdFigureToolBox, target_index, spell_data.spell_line_id))
+    {
+        apiFigureFunctions.addBonusMultToStatistic(_this->SF_CGdFigure, STRENGTH, target_index, spell_data.params[0]);
+    }
+    default_end_handler(_this, spell_index);
+}
+
+void __thiscall quickness_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
+{
+    uint16_t target_index = _this->active_spell_list[spell_index].target.entity_index;
+    int8_t bonus = (int8_t)(-apiSpellFunctions.getXData(_this, spell_index, SPELL_STAT_MUL_MODIFIER));
+
+    if (!apiToolboxFunctions.hasSpellOnHit(_this->SF_CGdFigureToolBox, target_index, spell_data.spell_line_id))
+    {
+        apiFigureFunctions.addBonusMultToStatistic(_this->SF_CGdFigure, WALK_SPEED, target_index, bonus);
+    }
+    default_end_handler(_this, spell_index);
+}
 void __thiscall flexibility_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
 void __thiscall strength_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
 void __thiscall retention_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
