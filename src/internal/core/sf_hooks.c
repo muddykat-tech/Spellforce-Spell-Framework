a08dc3a9d729f402c5115d4a48bf203fb1eb836b
UnSchtalch <shadow.93.04@gmail.com>
2024-05-30 19:17:54 +0300

    Console Log fix

--- src/internal/core/sf_hooks.c
+++ src/internal/core/sf_hooks.c
@@ -147,7 +147,7 @@ void initialize_console_hook()
 {
     uint32_t CAppMain_ptr = ASI::AddrOf(0x9229A8);
     uint32_t CAppMenu_ptr = *(uint32_t *)(CAppMain_ptr + 0x4);
-    uint32_t CMnuScrConsole_ptr = *(uint32_t *)(CAppMenu_ptr + 0x80);
+    CMnuScrConsole_ptr = *(uint32_t *)(CAppMenu_ptr + 0x80);
     console_print = (console_print_ptr)ASI::AddrOf(0x534e70);
     SF_String_ctor = (SF_String_ctor_ptr)ASI::AddrOf(0x3837e0);
     SF_String_dtor = (SF_String_dtor_ptr)ASI::AddrOf(0x3839c0);
