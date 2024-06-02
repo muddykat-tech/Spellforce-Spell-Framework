3abd67af652c895e415c3ef830b01bb6643139d5
UnSchtalch <shadow.93.04@gmail.com>
2024-06-02 13:12:49 +0300

    inablitiy handler

--- src/internal/handlers/sf_spellend_handlers.cpp
+++ src/internal/handlers/sf_spellend_handlers.cpp
@@ -137,7 +137,18 @@ void __thiscall suffocation_end_handler(SF_CGdSpell *_this, uint16_t spell_index
     default_end_handler(_this, spell_index);
 }
 
-void __thiscall inablility_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
+void __thiscall inablility_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
+{
+    SF_CGdResourceSpell spell_data;
+    spellAPI.getResourceSpellData(_this->SF_CGdResource, &spell_data, _this->active_spell_list[spell_index].spell_id);
+    uint16_t target_index = _this->active_spell_list[spell_index].target.entity_index;
+    if (toolboxAPI.hasSpellOnHit(_this->SF_CGdFigureToolBox, target_index, spell_data.spell_line_id))
+    {
+        figureAPI.addBonusMultToStatistic(_this->SF_CGdFigure, DEXTERITY, target_index, spell_data.params[0]);
+    }
+    default_end_handler(_this, spell_index);
+}
+
 void __thiscall slow_fighting_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
 void __thiscall dexterity_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
 void __thiscall edurance_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
@@ -150,4 +161,3 @@ void __thiscall firebane_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
 void __thiscall black_almightness_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
 void __thiscall mutation_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
 void __thiscall eternity_end_handler(SF_CGdSpell *_this, uint16_t spell_index);
-
