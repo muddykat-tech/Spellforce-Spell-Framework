e153b0749cc6b479db3edc3494fb7417ddd623f2
muddykat-tech <jarrod.muddyman@muddykat.tech>
2024-06-08 06:22:48 +0100

    Modified create-examples batch file to also move files to a single folder in root called 'examples-bin'

--- create-examples.bat
+++ create-examples.bat
@@ -1,13 +1,25 @@
 @echo off
 cd .\examples\
 
+echo "|=== Running Make on Example Mods ===|"
+
 for /D %%d in (*) do (
-	cd %%d
-	echo Making in %%d...
-	mingw32-make clean
-	mingw32-make
-	cd ..
+    cd %%d
+    echo "|--- Making %%d"
+    mingw32-make clean --quiet
+    mingw32-make --quiet
+    
+    REM Check if the "bin" folder exists in the current subfolder
+    if exist "bin" (
+        REM Create the destination directory in the root if it doesn't exist
+        if not exist "..\..\examples-bin" (
+            mkdir "..\..\examples-bin"
+        )
+        REM Copy the contents of the "bin" folder to the destination directory and overwrite existing files
+        xcopy "bin\*" "..\..\examples-bin\" /E /Y >nul
+    )
+    
+    cd ..
 )
 
-echo Finished Making Examples Mods
-pause
+echo "|=== Finished Making Example Mods ===|"
