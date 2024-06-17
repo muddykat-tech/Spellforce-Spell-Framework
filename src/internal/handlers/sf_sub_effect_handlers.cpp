3d7455864592badf5a578c7ad031bc1ec58d87d1
UnSchtalch <shadow.93.04@gmail.com>
2024-06-17 11:52:43 +0300

    Common and elemental chain handlers.

--- src/internal/handlers/sf_sub_effect_handlers.cpp
+++ src/internal/handlers/sf_sub_effect_handlers.cpp
@@ -55,3 +55,56 @@ void __thiscall elemental_sub_effect_handler(SF_CGDEffect *_this, uint16_t effec
         spellAPI.addSpell(_this->CGdSpell, spell_id, _this->OpaqueClass->current_step, &source, &target, 0);
     }
 }
+
+void __thiscall elemental_chain_sub_effect_handler(SF_CGDEffect *_this, uint16_t effect_index)
+{
+    uint16_t spell_id = effectAPI.getEffectXData(_this, effect_index, EFFECT_SUBSPELL_ID);
+    uint16_t figure_index1 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_INDEX);
+    uint8_t figure_type1;
+    if (figure_index1)
+    {
+        figure_type1 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_TYPE);
+    }
+    uint16_t figure_index2 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_INDEX2);
+    uint16_t figure_index3 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_INDEX3);
+    uint8_t figure_type2 = 0;
+    if (figure_index2)
+    {
+        figure_type2 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_TYPE2);
+    }
+    if (!figure_type2 || !figure_index2)
+    {
+        return;
+    }
+    SF_CGdTargetData source = {figure_type1, figure_index1, {0, 0}};
+    SF_CGdTargetData target = {figure_type2, figure_index2, {0, 0}};
+    spellAPI.addSpell(_this->CGdSpell, spell_id, _this->OpaqueClass->current_step, &source, &target, figure_index3);
+}
+
+void __thiscall common_sub_effect_handler (SF_CGDEffect *_this, uint16_t effect_index)
+{
+    uint16_t spell_id = effectAPI.getEffectXData(_this, effect_index, EFFECT_SUBSPELL_ID);
+    uint16_t figure_index1 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_INDEX);
+    uint8_t figure_type1;
+    if (figure_index1)
+    {
+        figure_type1 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_TYPE);
+    }
+    uint16_t figure_index2 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_INDEX2);
+    uint8_t figure_type2 = 0;
+    if (figure_index2)
+    {
+        figure_type2 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_TYPE2);
+    }
+    if (!figure_type2 || !figure_index2)
+    {
+        return;
+    }
+    if (!figure_type1 || !figure_index1)
+    {
+        return;
+    }
+    SF_CGdTargetData source = {figure_type1, figure_index1, {0, 0}};
+    SF_CGdTargetData target = {figure_type2, figure_index2, {0, 0}};
+    spellAPI.addSpell(_this->CGdSpell, spell_id, _this->OpaqueClass->current_step, &source, &target, 0);
+}
