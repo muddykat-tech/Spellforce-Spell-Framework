abef6a7ea9041e269deb0fcb91dfb234517a8b5c
UnSchtalch <shadow.93.04@gmail.com>
2024-09-28 19:14:06 +0300

    Internal passthrough of getLeveledSpellID + daamage fix

--- src/internal/core/hooks/sf_onhit_hook.c
+++ src/internal/core/hooks/sf_onhit_hook.c
@@ -14,16 +14,19 @@
 
 typedef uint16_t(__thiscall *get_reduced_damage_ptr)(void *AutoClass34, uint16_t source_index, uint16_t target_index, uint16_t unkn);
 typedef uint16_t(__thiscall *get_hit_chance_ptr)(void *AutoClass34, uint16_t source_index, uint16_t target_index);
+typedef uint16_t(__thiscall *get_leveled_spell_ptr)(void *CGdResource, uint16_t source_spell_id, uint16_t spell_level);
 
 typedef void(__thiscall *FUN_006c3a60_ptr)(void *AutoClass30, uint16_t source_index, uint16_t target_index, uint8_t unkn, uint32_t unkn2);
 
 get_reduced_damage_ptr g_get_reduced_damage;
 get_hit_chance_ptr g_get_hit_chance;
+get_leveled_spell_ptr g_get_leveled_spell;
 FUN_006c3a60_ptr g_FUN_006c3a60;
 void initialize_onhit_data_hooks()
 {
     g_get_reduced_damage = (get_reduced_damage_ptr)(ASI::AddrOf(0x3177d0));
     g_get_hit_chance = (get_hit_chance_ptr)(ASI::AddrOf(0x317860));
+    g_get_leveled_spell = (get_leveled_spell_ptr)(ASI::AddrOf(0x26de20));
     g_FUN_006c3a60 = (FUN_006c3a60_ptr)(ASI::AddrOf(0x2c3a60));
 }
 
@@ -196,64 +199,6 @@ void __thiscall sf_onhit_hook(SF_CGdFigureJobs *_this, uint16_t source_index, ui
 
     if (target.entity_type == 1)
     {
-        // donnoe wtf is it, looks like shooting?
-        if (action.type == 0x2712)
-        {
-            uint16_t maxY = (_this->CGdFigure->figures[source_index].position.Y <= _this->CGdFigure->figures[target.entity_index].position.Y)
-                                ? (_this->CGdFigure->figures[target.entity_index].position.Y)
-                                : (_this->CGdFigure->figures[source_index].position.Y);
-
-            uint16_t maxX = (_this->CGdFigure->figures[source_index].position.X <= _this->CGdFigure->figures[target.entity_index].position.X)
-                                ? (_this->CGdFigure->figures[target.entity_index].position.X)
-                                : (_this->CGdFigure->figures[source_index].position.X);
-
-            uint16_t minY = (_this->CGdFigure->figures[source_index].position.Y > _this->CGdFigure->figures[target.entity_index].position.Y)
-                                ? (_this->CGdFigure->figures[target.entity_index].position.Y)
-                                : (_this->CGdFigure->figures[source_index].position.Y);
-
-            uint16_t minX = (_this->CGdFigure->figures[source_index].position.X > _this->CGdFigure->figures[target.entity_index].position.X)
-                                ? (_this->CGdFigure->figures[target.entity_index].position.X)
-                                : (_this->CGdFigure->figures[source_index].position.X);
-            SF_Coord p1;
-            SF_Coord p2;
-            SF_Rectangle vector;
-            p1.X = maxX;
-            if (minX < maxX)
-            {
-                p1.X = minX;
-            }
-            p1.Y = maxY;
-            if (minY < maxY)
-            {
-                p1.Y = minY;
-            }
-
-            p2.X = maxX;
-            if (maxX <= minX)
-            {
-                p2.X = minX;
-            }
-            p2.Y = maxY;
-            if (maxY <= minY)
-            {
-                p2.Y = minY;
-            }
-            vector.partA = p1.Y << 0x10 | p1.X;
-            vector.partB = p2.Y << 0x10 | p2.X;
-
-            uint32_t distance = getDistange(&_this->CGdFigure->figures[source_index].position, &_this->CGdFigure->figures[target.entity_index].position);
-            distance = ((distance & 0xffff) * 0x578) / 3000;
-            SF_CGdTargetData source = {1, source_index, {0, 0}};
-            uint16_t effect_id = effectAPI.addEffect(_this->CGdEffect, kGdEffectProjectile, &source, &target,
-                                                     _this->OpaqueClass->current_step, ((distance != 0) ? distance : 1), &vector);
-            effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_ENTITY_TYPE, 1);
-            effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_ENTITY_TYPE2, 1);
-            effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_ENTITY_INDEX, source_index);
-            effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_ENTITY_INDEX2, target.entity_index);
-            effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_DO_NOT_ADD_SUBSPELL, 1);
-            effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_PHYSICAL_DAMAGE, 0);
-        }
-
         if ((_this->CGdFigure->figures[target.entity_index].owner == -1) ||
             (_this->CGdFigure->figures[target.entity_index].flags & 10 != 0) ||
             (!toolboxAPI.isTargetable(_this->CGdFigureToolBox, target.entity_index)))
@@ -313,7 +258,7 @@ void __thiscall sf_onhit_hook(SF_CGdFigureJobs *_this, uint16_t source_index, ui
                 uint16_t list_size = onhit_list.size();
 
                 char ogdamage_info[128];
-                snprintf(ogdamage_info, sizeof(ogdamage_info), "Before OnHit: %d", weapon_damage);
+                snprintf(ogdamage_info, sizeof(ogdamage_info), "Before OnHit: %d", damage);
                 log_info(ogdamage_info);
 
                 for (auto it = onhit_list.crbegin(); it != onhit_list.crend(); ++it)
@@ -334,7 +279,7 @@ void __thiscall sf_onhit_hook(SF_CGdFigureJobs *_this, uint16_t source_index, ui
                             if (toolboxAPI.hasSpellOnIt(_this->CGdFigureToolBox, target.entity_index, spell_line_id))
                             {
                                 onhit_handler_ptr onhit_func = entry.second;
-                                weapon_damage = onhit_func(_this, source_index, target.entity_index, weapon_damage);
+                                damage = onhit_func(_this, source_index, target.entity_index, damage);
                             }
                         }
                     }
@@ -345,16 +290,101 @@ void __thiscall sf_onhit_hook(SF_CGdFigureJobs *_this, uint16_t source_index, ui
                             if (toolboxAPI.hasSpellOnIt(_this->CGdFigureToolBox, source_index, spell_line_id))
                             {
                                 onhit_handler_ptr onhit_func = entry.second;
-                                weapon_damage = onhit_func(_this, source_index, target.entity_index, weapon_damage);
+                                damage = onhit_func(_this, source_index, target.entity_index, damage);
                             }
                         }
                     }
                 }
 
                 char damage_info[128];
-                snprintf(damage_info, sizeof(damage_info), "OnHit: %d", weapon_damage);
+                snprintf(damage_info, sizeof(damage_info), "OnHit: %d", damage);
                 log_info(damage_info);
-                toolboxAPI.dealDamage(_this->CGdFigureToolBox, source_index, target.entity_index, weapon_damage, weapon_damage, 0, 0);
+                // donnoe wtf is it, looks like shooting?
+                if (action.type == 0x2712)
+                {
+                    uint16_t maxY = (_this->CGdFigure->figures[source_index].position.Y <= _this->CGdFigure->figures[target.entity_index].position.Y)
+                                        ? (_this->CGdFigure->figures[target.entity_index].position.Y)
+                                        : (_this->CGdFigure->figures[source_index].position.Y);
+
+                    uint16_t maxX = (_this->CGdFigure->figures[source_index].position.X <= _this->CGdFigure->figures[target.entity_index].position.X)
+                                        ? (_this->CGdFigure->figures[target.entity_index].position.X)
+                                        : (_this->CGdFigure->figures[source_index].position.X);
+
+                    uint16_t minY = (_this->CGdFigure->figures[source_index].position.Y > _this->CGdFigure->figures[target.entity_index].position.Y)
+                                        ? (_this->CGdFigure->figures[target.entity_index].position.Y)
+                                        : (_this->CGdFigure->figures[source_index].position.Y);
+
+                    uint16_t minX = (_this->CGdFigure->figures[source_index].position.X > _this->CGdFigure->figures[target.entity_index].position.X)
+                                        ? (_this->CGdFigure->figures[target.entity_index].position.X)
+                                        : (_this->CGdFigure->figures[source_index].position.X);
+                    SF_Coord p1;
+                    SF_Coord p2;
+                    SF_Rectangle vector;
+                    p1.X = maxX;
+                    if (minX < maxX)
+                    {
+                        p1.X = minX;
+                    }
+                    p1.Y = maxY;
+                    if (minY < maxY)
+                    {
+                        p1.Y = minY;
+                    }
+
+                    p2.X = maxX;
+                    if (maxX <= minX)
+                    {
+                        p2.X = minX;
+                    }
+                    p2.Y = maxY;
+                    if (maxY <= minY)
+                    {
+                        p2.Y = minY;
+                    }
+                    vector.partA = p1.Y << 0x10 | p1.X;
+                    vector.partB = p2.Y << 0x10 | p2.X;
+
+                    uint32_t distance = getDistange(&_this->CGdFigure->figures[source_index].position, &_this->CGdFigure->figures[target.entity_index].position);
+                    distance = ((distance & 0xffff) * 0x578) / 3000;
+                    SF_CGdTargetData source = {1, source_index, {0, 0}};
+                    uint16_t effect_id = effectAPI.addEffect(_this->CGdEffect, kGdEffectProjectile, &source, &target,
+                                                             _this->OpaqueClass->current_step, ((distance != 0) ? distance : 1), &vector);
+                    effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_ENTITY_TYPE, 1);
+                    effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_ENTITY_TYPE2, 1);
+                    effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_ENTITY_INDEX, source_index);
+                    effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_ENTITY_INDEX2, target.entity_index);
+                    effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_DO_NOT_ADD_SUBSPELL, 1);
+                    effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_PHYSICAL_DAMAGE, damage);
+                    uint16_t subspell_id = 0;
+                    // troll fire thrower
+                    if (_this->CGdFigure->figures[source_index].unit_data_id == 0x508)
+                    {
+                        subspell_id = 0x6de;
+                    }
+                    // troll ice thrower
+                    if (_this->CGdFigure->figures[source_index].unit_data_id == 0x505)
+                    {
+                        subspell_id = 0x6ea;
+                    }
+                    // windarcher
+                    if (_this->CGdFigure->figures[source_index].unit_data_id == 0x4cd)
+                    {
+                        subspell_id = 0x6d2;
+                    }
+                    if (subspell_id != 0)
+                    {
+                        uint16_t spell_lvl = _this->CGdFigure->figures[source_index].level;
+                        uint16_t spell_id = g_get_leveled_spell(_this->CGdResource, subspell_id, spell_lvl);
+                        if (spell_id != 0)
+                        {
+                            effectAPI.setEffectXData(_this->CGdEffect, effect_id, EFFECT_SUBSPELL_ID, spell_id);
+                        }
+                    }
+                }
+                else
+                {
+                    toolboxAPI.dealDamage(_this->CGdFigureToolBox, source_index, target.entity_index, damage, 0, 0, 0);
+                }
                 // logic here:
                 //  calculate modification from spells that increase damage
 
