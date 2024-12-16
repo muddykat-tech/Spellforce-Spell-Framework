92c87dd780b62f7753db2fa512f2c1427dc49131
muddykat-tech <jarrod.muddyman@muddykat.tech>
2024-12-16 09:38:26 +0000

    Added the LuaConverter.py, Used to create a string version of the ASI file for use in our unpacker for the Steam Workshop version of SFSF

---
+++ luaconverter.py
@@ -0,0 +1,33 @@
+import getopt, sys, os
+
+def translate(fname):
+    f = open(fname, mode="rb")
+    s = f.read()
+    f.close()
+
+    b = []
+    for c in s:
+        b.append("\\{:03n}".format(c))
+
+    s2 = ''.join(b)
+
+    fname2 = os.path.splitext(fname)[0]+"_result.txt"
+    print(fname, fname2)
+    f = open(fname2, mode="w", encoding = "ascii")
+    f.write(s2)
+    f.close()
+    
+
+args = sys.argv[1:]
+
+options = "f:"
+
+try:
+    arguments, values = getopt.getopt(args, options)
+
+    for c in arguments:
+        if c[0] == "-f":
+            print(arguments, c)
+            translate(c[1])
+except getopt.error as err:
+    print(str(err))
