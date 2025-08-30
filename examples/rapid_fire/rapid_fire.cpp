3161834e98ae8bed96d6165ee036d1208ea56b24
UnSchtalch <shadow.93.04@gmail.com>
2025-08-31 01:17:46 +0300

    Critical hits bugfix. VSCode shenanigans with multiple auras fix Some tests with examples

--- examples/rapid_fire/rapid_fire.cpp
+++ examples/rapid_fire/rapid_fire.cpp
@@ -13,19 +13,69 @@ ToolboxFunctions *toolboxAPI;
 FigureFunctions *figureAPI;
 RegistrationFunctions *registrationAPI;
 SFLog *logAPI;
+EffectFunctions *effectAPI;
+AiFunctions *aiAPI;
+
+bool __thiscall isActionMelee(SF_SGtFigureAction *_this)
+{
+    if ((_this->type == 10000) || (_this->type == 0x2711))
+    {
+        return 1;
+    }
+    return 0;
+}
 
 
 void __thiscall rapid_fire_type_handler(SF_CGdSpell *_this, uint16_t spell_index)
 {
     _this->active_spell_list[spell_index].spell_job = RAPID_FIRE_JOB;
     spellAPI->setXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 0);
-    spellAPI->setXData(_this, spell_index, SPELL_STAT_MUL_MODIFIER, 0);
 }
 
 void __thiscall rapid_fire_end_handler(SF_CGdSpell *_this, uint16_t spell_index)
 {
+    spellAPI->figTryClrCHkSPlBfrJob2(_this, spell_index);
+    spellAPI->figClrChkSplBfrChkBattle(_this, spell_index, 0);
     spellAPI->removeDLLNode(_this, spell_index);
     spellAPI->setEffectDone(_this, spell_index, 0);
+}
+
+uint16_t __thiscall rapid_fire_onhit_handler(SF_CGdFigureJobs *_this,
+                                             uint16_t source_index,
+                                             uint16_t target_index, uint16_t damage)
+{
+    SF_SGtFigureAction action;
+    aiAPI->getTargetAction(_this->CGdFigure, &action, source_index);
+    bool isMeleeAttack = isActionMelee(&action);
+    //logAPI->logInfo("On Hit action %d", action.type);
+    if (!isMeleeAttack)
+    {
+        return damage;
+    }
+    SF_CGdResourceSpell spell_data;
+    uint16_t spell_index = toolboxAPI->getSpellIndexOfType(_this->CGdFigureToolBox, source_index, rapid_fire_LINE, 0);
+    if (spell_index == 0)
+    {
+        return damage;
+    }
+    SF_GdSpell *spell = &_this->CGdSpell->active_spell_list[spell_index];
+    uint16_t spell_id = spellAPI->getSpellID(_this->CGdSpell, spell_index);
+    spellAPI->getResourceSpellData(_this->CGdResource, &spell_data, spell_id);
+
+    SF_SpellEffectInfo effect_info;
+    effect_info.spell_id = spell->spell_id;
+    effect_info.job_id = spell->spell_job;
+
+    uint16_t resistance = spellAPI->getChanceToResistSpell(_this->CGdSpell->unkn2, source_index, target_index,
+                                                           effect_info);
+    uint16_t random = spellAPI->getRandom(_this->OpaqueClass, 100);
+    //logAPI->logInfo("On Hit active %d %d", resistance, random);
+    if (resistance < random)
+    {
+        toolboxAPI->dealDamage(_this->CGdFigureToolBox, source_index, target_index, spell_data.params[2], 1, 0, 0);
+    }
+    figureAPI->subMana(_this->CGdFigure, source_index, spell_data.params[1]);
+    return damage;
 }
 
 void __thiscall rapid_fire_effect_handler(SF_CGdSpell *_this, uint16_t spell_index)
@@ -34,25 +84,39 @@ void __thiscall rapid_fire_effect_handler(SF_CGdSpell *_this, uint16_t spell_ind
     uint16_t source_index = spell->source.entity_index;
     SF_CGdResourceSpell spell_data;
     spellAPI->getResourceSpellData(_this->SF_CGdResource, &spell_data, spell->spell_id);
+    SF_CGdTargetData source_data;
+    source_data.entity_index = source_index;
+    source_data.entity_type = 1;
+    source_data.position = {0,0};
     spell->to_do_count = (spell_data.params[0] * 10) / 1000;
     uint32_t tick = spellAPI->addToXData(_this, spell_index, SPELL_TICK_COUNT_AUX, 1);
     if (tick == 1)
     {
-        if (_this->SF_CGdFigure->figures[source_index].weapon_stats[1].max_rng > 2)
-        {
-            uint32_t wspeed = _this->SF_CGdFigure->figures[source_index].weapon_stats[1].wpn_spd;
-            spellAPI->setXData(_this, spell_index, SPELL_STAT_MUL_MODIFIER, wspeed);
-            wspeed = (wspeed * (100 + spell_data.params[1])) / 100; //percentage increase
-            _this->SF_CGdFigure->figures[source_index].weapon_stats[1].wpn_spd = wspeed;
-        }
+        _this->SF_CGdFigure->figures[source_index].flags |= AURA_RUNNING;
+        _this->SF_CGdFigure->figures[source_index].flags |= F_CHECK_SPELLS_BEFORE_JOB;
+        _this->active_spell_list[spell_index].flags |= CHECK_SPELLS_BEFORE_JOB2;
+        SF_Rectangle rect = {0, 0};
+        uint16_t effect_index = effectAPI->addEffect(_this->SF_CGdEffect, kGdEffectSpellHitTarget, &source_data,
+                                                     &source_data,
+                                                     _this->OpaqueClass->current_step, 0, &rect);
+
+        spellAPI->setXData(_this, spell_index, EFFECT_EFFECT_INDEX, effect_index);
+        effectAPI->setEffectXData(_this->SF_CGdEffect, effect_index, EFFECT_SPELL_INDEX, spell_index);
+    }
+    uint16_t current_mp = figureAPI->getCurrentStat(_this->SF_CGdFigure, source_index, MANA);
+    uint16_t manacost = spell_data.params[1];
+    if (current_mp >= manacost)
+    {
         return;
     }
-    if (_this->SF_CGdFigure->figures[source_index].weapon_stats[1].max_rng > 2)
+    if (source_index != 0)
     {
-        _this->SF_CGdFigure->figures[source_index].weapon_stats[1].wpn_spd = spellAPI->getXData(_this, spell_index,
-                                                                                                SPELL_STAT_MUL_MODIFIER);
+        _this->SF_CGdFigure->figures[source_index].flags &= ~AURA_RUNNING;
     }
-    spellAPI->removeDLLNode(_this, spell_index);
+
+    uint16_t effect_index = spellAPI->getXData(_this, spell_index, EFFECT_EFFECT_INDEX);
+    effectAPI->tryEndEffect(_this->SF_CGdEffect, effect_index);
+    spellAPI->setXData(_this, spell_index, EFFECT_EFFECT_INDEX, 0);
     spellAPI->setEffectDone(_this, spell_index, 0);
 }
 
@@ -70,11 +134,15 @@ extern "C" __declspec(dllexport) void InitModule(SpellforceSpellFramework *frame
     figureAPI = sfsf->figureAPI;
     registrationAPI = sfsf->registrationAPI;
     logAPI = sfsf->logAPI;
+    effectAPI = sfsf->effectAPI;
+    aiAPI = sfsf->aiAPI;
 
     SFSpell *rapid_fire_spell = registrationAPI->registerSpell(rapid_fire_LINE);
     registrationAPI->linkTypeHandler(rapid_fire_spell, &rapid_fire_type_handler);
     registrationAPI->linkEffectHandler(rapid_fire_spell, RAPID_FIRE_JOB, &rapid_fire_effect_handler);
     registrationAPI->linkEndHandler(rapid_fire_spell, &rapid_fire_end_handler);
+    registrationAPI->applySpellTag(rapid_fire_spell,SpellTag::AURA_SPELL);
+    registrationAPI->linkOnHitHandler(rapid_fire_spell, &rapid_fire_onhit_handler, PHASE_4);
 }
 
 
@@ -85,8 +153,8 @@ extern "C" __declspec(dllexport) void InitModule(SpellforceSpellFramework *frame
 
 extern "C" __declspec(dllexport) SFMod *RegisterMod(SpellforceSpellFramework *framework)
 {
-    return framework->createModInfo("The custom spell rapid_fire", "1.0.0", "Muddykat, UnSchtalch, Teekius",
-                                    "Weapon stat modification test");
+    return framework->createModInfo("The custom spell rapid_fire", "1.0.0", "UnSchtalch",
+                                    "On hit aura test");
 }
 
 
