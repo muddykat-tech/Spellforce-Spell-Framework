3bd0eb31796580a1a412dc03d1227d934c46c669
UnSchtalch <shadow.93.04@gmail.com>
2026-01-13 11:21:22 +0300

    Yet another -Wall cleanup

--- src/internal/handlers/sf_sub_effect_handlers.cpp
+++ src/internal/handlers/sf_sub_effect_handlers.cpp
@@ -71,21 +71,17 @@ void __thiscall elemental_chain_sub_effect_handler(SF_CGDEffect *_this,
                                                  EFFECT_SUBSPELL_ID);
     uint16_t source_index = effectAPI.getEffectXData(_this, effect_index,
                                                      EFFECT_ENTITY_INDEX);
-    uint8_t figure_type1;
+    uint8_t figure_type1 = 0;
     if (source_index)
     {
-        figure_type1 = effectAPI.getEffectXData(_this, effect_index,
-                                                EFFECT_ENTITY_TYPE);
+        figure_type1 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_TYPE);
     }
-    uint16_t figure_index2 = effectAPI.getEffectXData(_this, effect_index,
-                                                      EFFECT_ENTITY_INDEX2);
-    uint16_t figure_index3 = effectAPI.getEffectXData(_this, effect_index,
-                                                      EFFECT_ENTITY_INDEX3);
+    uint16_t figure_index2 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_INDEX2);
+    uint16_t figure_index3 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_INDEX3);
     uint8_t figure_type2 = 0;
     if (figure_index2)
     {
-        figure_type2 = effectAPI.getEffectXData(_this, effect_index,
-                                                EFFECT_ENTITY_TYPE2);
+        figure_type2 = effectAPI.getEffectXData(_this, effect_index, EFFECT_ENTITY_TYPE2);
     }
     if (!figure_type2 || !figure_index2)
     {
@@ -93,9 +89,7 @@ void __thiscall elemental_chain_sub_effect_handler(SF_CGDEffect *_this,
     }
     SF_CGdTargetData source = {figure_type1, source_index, {0, 0}};
     SF_CGdTargetData target = {figure_type2, figure_index2, {0, 0}};
-    spellAPI.addSpell(_this->CGdSpell, spell_id,
-                      _this->OpaqueClass->current_step, &source, &target,
-                      figure_index3);
+    spellAPI.addSpell(_this->CGdSpell, spell_id, _this->OpaqueClass->current_step, &source, &target, figure_index3);
 }
 
 void __thiscall common_sub_effect_handler(SF_CGDEffect *_this,
